35f330897b0fa7e5126ee7ffaa362bcdeb2ca334
Hadekha Erfadila Fitra <hdkef11@gmail.com>
2021-04-18 20:09:50 +0800

    add responsive hamburger using eventlistener

--- src/app/feed/main/main.component.ts
+++ src/app/feed/main/main.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
+  ngOnDestroy(): void {
+    window.removeEventListener("resize",()=>{})
+  }
+
   ngOnInit(): void {
+    window.addEventListener("resize", function(event) {
+      if (window.matchMedia('screen and (max-width: 1000px)').matches) {
+        document.getElementById("left").style.display = "none";
+      }else{
+        document.getElementById("left").style.display = "block";
+      }
+  })
+  }
+
+  hamburgerListener($value){
+    if ($value == true){
+      document.getElementById("left").style.display = "block";
+    }else{
+      document.getElementById("left").style.display = "none"
+    }
   }
 
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  open = false
+
+  @Output() hamburger = new EventEmitter<boolean>()
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  hamburgerEmitter(){
+    this.open = !this.open
+    this.hamburger.emit(this.open)
+  }
+
+
 
 }
